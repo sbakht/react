63f8157ac17993f257c89c1e0843194bf728380b
Saad Bakht <rotk13@gmail.com>
2017-08-25 17:37:00 -0400

    test simple maadhi table

--- src/questionBuilder.test.js
+++ src/questionBuilder.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {QuestionBuilder, MaadhiQuestion} from './QuestionBuilder';
 import Util from './util';
+import _ from 'underscore';
 
 test('Link changes the class when hovered', () => {
   Util.Root.getRandom = jest.fn();
@@ -20,3 +21,51 @@ test('Link changes the class when hovered', () => {
 
   expect(question.question).toMatchSnapshot();
 });
+
+test('builds simple maadhi table', () => {
+  var endings = ['','ا','وا','ت','تا','ن','ت','تما','تم','ت','تما','تن','ت','نا']
+  var f = 'َ';
+  var d = 'ُ';
+  var k = 'ِ';
+  var s = 'ْ';
+  var sh ='ّ';
+  var mapping = [[2], [0,2], [1], [2,4], [2,2], [4,2], [4,2], [4,1,2], [4,2,4], [2,3], [4,2,2], [4,1,5,2], [4,1], [4,2]];
+
+  mapping = mapping.map(function(m) {
+    return [2,2].concat(m);
+  });
+  var vowels = mapping.map(m => m.map(replacer));
+
+  var str = "فعل"
+  var words = [];
+
+  endings.map(function(ending, i) {
+    var word = str + ending;
+    var useVowels = vowels[i] || ['','',''];
+    words.push(_.flatten(_.zip(word.split(''), useVowels)).join(''));
+  });
+
+  function replacer(i) {
+    if(i === 0) {
+      return '';
+    }
+    if(i === 1) {
+      return d;
+    }
+    if(i === 2) {
+      return f;
+    }
+    if(i === 3) {
+      return k;
+    }
+    if(i === 4) {
+      return s;
+    }
+    if(i === 5) {
+      return sh;
+    }
+  }
+
+  expect(words).toMatchSnapshot();
+});
+
