9154e582e150656ec063176e2f585ccdd4c461c8
Saad Bakht <saad.bakht@bloomingdales.com>
2017-09-08 16:37:38 -0400

    infatara

--- src/table.test.js
+++ src/table.test.js
@@ -197,3 +197,34 @@ describe("istansara", () => {
         });
     });
 });
+
+
+describe("infatara", () => {
+
+    var options = { letters : "فعل", advanced: true, group : "maadhi", type : "type6"}
+    describe("maadhi", () => {
+        test('active', () => {
+          var table = new Table(options);
+          expect(table.words.active).toMatchSnapshot();
+        });
+
+        test('passive', () => {
+          var table = new Table(options);
+          expect(table.words.passive).toMatchSnapshot();
+        });
+    });
+
+    describe("mudari", () => {
+        test('active', () => {
+          options.group = "mudari";
+          var table = new Table(options);
+          expect(table.words.active).toMatchSnapshot();
+        });
+
+        test('passive', () => {
+          options.group = "mudari";
+          var table = new Table(options);
+          expect(table.words.passive).toMatchSnapshot();
+        });
+    });
+});
